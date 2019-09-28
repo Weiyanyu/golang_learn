ebfbbb87093792ef8656787241ab50dcde7f7acb
Weiyanyu <chinayeonon@gmail.com>
2019-09-28 23:39:33 +0800

    finish 1.2 section

---
+++ ch1/echo2.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main2() {
+	s, sep := "", ""
+
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+
+	fmt.Println(s)
+}
---
+++ ch1/exercise/1_3.go
@@ -0,0 +1,37 @@
+/*
+练习 1.3： 做实验测量潜在低效的版本和使用了strings.Join的版本的运行时间差异。
+（1.6节讲解了部分time包，11.4节展示了如何写标准测试程序，以得到系统性的性能评测。）
+ */
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now().Nanosecond()
+	lowEcho()
+	end := time.Now().Nanosecond()
+	fmt.Printf("low Echo version need %d ns\n", end - start)
+
+	start = time.Now().Nanosecond()
+	coolEcho()
+	end = time.Now().Nanosecond()
+	fmt.Printf("cool Echo version need %d ns\n", end - start)
+}
+
+func lowEcho() {
+	s, sep := "", ""
+
+	for _, arg := range os.Args[1: ] {
+		s += sep + arg
+	}
+}
+
+func coolEcho() {
+	strings.Join(os.Args[1:], " ")
+}
