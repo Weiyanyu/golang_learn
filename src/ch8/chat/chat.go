66794127deb192947dcdcea572805e69d09201f3
Weiyanyu <chinayeonon@gmail.com>
2019-12-18 23:23:30 +0800

    finish chapter 8

---
+++ src/ch8/chat/chat.go
@@ -0,0 +1,81 @@
+package chat
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+type client chan<- string
+
+var (
+	messages = make(chan string)
+	entering = make(chan client)
+	leaving  = make(chan client)
+)
+
+func broadcaster() {
+	clients := make(map[client]bool)
+	for {
+		select {
+		case msg := <-messages:
+			for client := range clients {
+				select {
+				case client <- msg:
+				default:
+					continue
+				}
+
+			}
+		case cli := <-entering:
+			clients[cli] = true
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli)
+		}
+	}
+}
+
+func handleConn(conn net.Conn) {
+	ch := make(chan string)
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String()
+	ch <- "You are " + who
+	messages <- who + "arrivied, welcome"
+	entering <- ch
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- who + " : " + input.Text()
+	}
+
+	leaving <- ch
+	messages <- who + " has left"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg)
+	}
+}
+
+func TestChat() {
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
--- src/ch8/main.go
+++ src/ch8/main.go
@@ -2,56 +2,13 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"go_learn/src/ch8/du"
-	"sync"
-	"time"
+	"go_learn/src/ch8/chat"
 )
 
 var verbose = flag.Bool("v", false, "show verbose progress messages")
 
 func main() {
-	flag.Parse()
-	roots := flag.Args()
-	if len(roots) == 0 {
-		roots = []string{"."}
-	}
-
-	fileSizes := make(chan int64)
-	var w sync.WaitGroup
-
-	for _, root := range roots {
-		w.Add(1)
-		go du.Walk(root, &w, fileSizes)
-	}
-
-	go func() {
-		w.Wait()
-		close(fileSizes)
-	}()
-
-	var tick <-chan time.Time
-	if *verbose {
-		tick = time.Tick(100 * time.Millisecond)
-	}
-	var nfiles, nbytes int64
-
-loop:
-
-	for {
-		select {
-		case size, ok := <-fileSizes:
-			if !ok {
-				break loop
-			}
-			nbytes += size
-			nfiles += 1
-		case <-tick:
-			fmt.Printf("%d files %.1f GB\n", nfiles, float64(nbytes)/1e9)
-		}
-	}
-
-	fmt.Printf("%d files %.1f GB\n", nfiles, float64(nbytes)/1e9)
+	chat.TestChat()
 }
 
 var tokens = make(chan struct{}, 20)
